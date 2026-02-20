85cf1901b9cf5b4f2978e5ade9e1974cb2011ec4
VIKRAM KUMAR D <25aimb59@kristujayanti.com>
2026-02-20 21:12:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     try {
       // Send to backend
-      const res = await fetch("http://localhost:500", {
+      const res = await fetch("http://localhost:5000/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, message }),
