43534221cbf4498746f75e750ee8456dab3d33a1
VIKRAM KUMAR D <25aimb59@kristujayanti.com>
2026-02-21 13:34:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     try {
       // POST to backend
-      const res = await fetch("https://vikram-portfolio-mgq7.onrender.com/contact", {
+      const res = await fetch("http://localhost:5000/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, message }),
